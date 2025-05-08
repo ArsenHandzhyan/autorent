0b54523aa1cef1bdf2ed7b33e7e43780d59c69c2
Администратор <Arsenhandzan442@gmail.com>
2025-05-09 02:09:34 +0300

    приложение для проката авто

--- src/main/java/ru/anapa/autorent/controller/AdminRentalController.java
+++ src/main/java/ru/anapa/autorent/controller/AdminRentalController.java
@@ -3,6 +3,7 @@ package ru.anapa.autorent.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -14,6 +15,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/admin/rentals") // Изменяем базовый URL
 @PreAuthorize("hasRole('ADMIN')")
+@Transactional
 public class AdminRentalController {
 
     private final RentalService rentalService;
